db83c59733dacc0ebf834f6764f3d53558134db2
ivymitchelle <ivygathingi@gmail.com>
2023-04-15 04:35:47 -0700

    My C assignment

---
+++ 0x09-static_libraries/1-memcpy.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "main.h"
+/*Copyinmg memory
+ *
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+}
---
+++ 0x09-static_libraries/3-strcmp.c
@@ -0,0 +1,9 @@
+#include "main.h"
+#include <stdio.h>
+int _strcmp(char *s1, char *s2)
+{
+}
+
+
+
+
